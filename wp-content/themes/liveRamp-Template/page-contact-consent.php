9e0bafee49f8d2c882426476fa7f5b77caccd998
Kumar Desai <kumar.desai@liveramp.com>
2019-12-04 10:36:50 -0800

    templatized the contact consent template

--- wp-content/themes/liveRamp-Template/page-contact-consent.php
+++ wp-content/themes/liveRamp-Template/page-contact-consent.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The template for displaying CPT archive pages
- * Template Name: Liveramp Contact Page Live Test
+ * Template Name: Liveramp Contact Consent
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages and that
@@ -54,89 +54,8 @@ get_header(); ?>
 							
 							<div class="caption dark-slate margin-bottom-1">All fields required * </div>
 							
-<!-- //////////////////////////////////////////////////////// -->
+							<?php get_template_part( 'template-parts/contact-consent', 'none' ); ?>			
 
-    <!-- onclick Function to consent for  marketo only -->
-    <script>
-        function accept__mkto() {
-            __cmp('acceptAll', {
-                purposeIds: [1, 2, 3, 4, 5],
-                vendorIds: [0]
-            }, function (data) {});
-            __cmp('acceptAllAdditional', {
-                vendorIds: [90]
-            }, function (data) {});
-        }
-    </script>
-
-    <!-- loading the form from mkto_js -->
-    <script>
-        function mkto_F() {
-            console.log('here');
-            var mkto_S = document.createElement('script');
-            mkto_S.type = 'text/javascript';
-            mkto_S.src = 'wp-content/themes/liveRamp-Template/mkto-forms/mkto_consent.js';
-            document.getElementById('mkto_consent').appendChild(mkto_S);
-        }
-    </script>
-
-    <!--  consent logic check for marketo -->
-    <script>
-        var consentForMarketoForm;
-        console.log('consentForMarketoForm is ', consentForMarketoForm);
-
-        function checkMktoConsent() {
-            window.__cmp('getAdditionalVendorConsents', undefined, function (data) {
-                var newConsentForMarketo = (data.purposeConsents[1] && data.vendorConsents[90] && data.purposeConsents[2] && data.purposeConsents[3] && data.purposeConsents[4] && data.purposeConsents[5]);
-                console.log('newConsentForMarketo is ', newConsentForMarketo);
-                if (consentForMarketoForm !== newConsentForMarketo) {
-                    consentForMarketoForm = newConsentForMarketo;
-                    if (consentForMarketoForm) {
-                        document.getElementById("consent_panel").style.display = "none";
-                        mkto_F();
-                    }
-                }
-            });
-        }
-    </script>
-    <!-- end of consent logic check for marketo -->
-
-    <!-- Button div -->
-    <div id="consent_panel" class="consent_panel_css">
-        <button id="mkto_btn" class="consent_Btn mktoButton button cta" onclick="accept__mkto();">Accept</button>
-        <p>Contact form not showing? You may Need to Update your consent preferences. Click here to give consent for Marketo.</p>
-    </div>
-
-    <!-- provided from mkto embedded script can call forms.min from marketo original link -->
-    <script src="https://lp.liveramp.com/js/forms2/js/forms2.min.js"></script>
-    <form id="mktoForm_1004"></form>
-
-    <!-- added this div with ID=mkto_consent to call the marketo form callback once the consent check is done-->
-    <div id="mkto_consent"></div>
-
-    <!-- If you have this logic implemented on the page then you only need to add checkMktoConsent() to checkConsentDataWithCallback()-->
-    <script>
-        window.__cmp('addEventListener', 'cmpReady', function () {
-            checkConsentDataWithCallback();
-        });
-        window.__cmp('addEventListener', 'consentChanged', function () {
-            checkConsentDataWithCallback();
-        });
-        // If false it means that the visitor is new and didnt give an action for consent 
-        // If true it means that the consent data object exist and we fire the check for the facebook consent  -->
-
-        function checkConsentDataWithCallback() {
-            window.__cmp('consentDataExist', true, function (consentDataExist) {
-                if (consentDataExist) {
-                    checkMktoConsent();
-                }
-            });
-        }
-    </script>
-
-
-<!-- //////////////////////////////////////////////////////// -->
-						
 						</div>
 					</div>
 				</div>
