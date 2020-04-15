56b4f390bac46867986966212c00837ca3d51f7c
Kumar Desai <kumar.desai@liveramp.com>
2020-04-15 09:54:17 -0700

    three ctas

---
+++ wp-content/themes/liveRamp-Template/acf-modules/three-ctas.php
@@ -0,0 +1,22 @@
+<div class="bm-new-section grid-container"> 
+   <div class="bm-section-outer"> 
+    <div class="bm-section-inr"> 
+        <h2 class="bm-heading green">Our Platform</h2> 
+        <p class="bm-content">Identity resolution is at the heart of what we do. With our platform, you can architect and own the single source of truth for customer data across the enterprise.</p> 
+        <a href="http://liveramp.com/our-platform/" class="bm-cta">See how it works</a> 
+    </div> 
+
+    <div class="bm-section-inr"> 
+        <h2 class="bm-heading green">Solution Finder</h2> 
+        <p class="bm-content">Tell us the type of work you do and the goal you\re trying to achieve. We\ll give you the tools to help take your marketing to the next level. Give it a whirl!</p> 
+        <a href="http://liveramp.com/solution-finder/" class="bm-cta">See your results</a> 
+    </div> 
+
+    <div class="bm-section-inr"> 
+        <h2 class="bm-heading green">Customer Stories</h2> 
+        <p class="bm-content">We love our customers (and our customers love us)! View stories from our customers that show the power of our platform and what LiveRamp can do for you. </p> 
+        <a href="http://liveramp.com/customer-stories/" class="bm-cta">See what customers say</a> 
+    </div> 
+    </div> 
+</div>
+
