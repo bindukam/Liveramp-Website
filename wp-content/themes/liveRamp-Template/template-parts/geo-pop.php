3f853ae9e73138400594d8663df0c098f3da1e6c
Neelam Thakur <neelam.thakur@nvish.local>
2020-05-05 17:28:39 +0530

    geo pop fixes

--- wp-content/themes/liveRamp-Template/template-parts/geo-pop.php
+++ wp-content/themes/liveRamp-Template/template-parts/geo-pop.php
@@ -82,88 +82,95 @@ $countryLabelList = [
 ];
 
 if(array_key_exists($countryCode,$countryLabelList)){ 
-	$orgUrl = site_url(); 
-	$expUrls = explode(".com", $orgUrl);
-	$expUrls = $expUrls[0];
+	$orgUrl = network_home_url(); 
+	//$orgUrl = site_url(); 
+	//$network_home_url = network_home_url(); 
+	$curPageURL = get_page_link(); 
+	if(chop($orgUrl,"/") == chop($curPageURL,"/")){
+		$expUrls = explode(".com", $orgUrl);
+		$expUrls = $expUrls[0];
 
-	switch ($countryCode) {
-		case 'FR':
-			$refer_to_site_url = $expUrls.'.fr/';
-			break;
-		case 'GB':
-			$refer_to_site_url = $expUrls.'.uk/';
-			break;
-		case 'AU':
-			$refer_to_site_url = $orgUrl.'.au/';
-			break;
-		case 'JP':
-			$refer_to_site_url = $expUrls.'.co.jp/';
-			break;
-		case 'DE':
-			$refer_to_site_url = $orgUrl.'/lr-de/';
-			break;
-		case 'ES':
-			$refer_to_site_url = $orgUrl.'/lr-es/';
-			break;
-		case 'IT':
-			$refer_to_site_url = $orgUrl.'/lr-it/';
-			break;
-		case 'CN':
-			$refer_to_site_url = $orgUrl.'.cn/';
-			break;
-		default:
-			$refer_to_site_url = $orgUrl;
-	} 
+		switch ($countryCode) {
+			case 'FR':
+				$refer_to_site_url = $expUrls.'.fr/';
+				break;
+			case 'GB':
+				$refer_to_site_url = $expUrls.'.uk/';
+				break;
+			case 'AU':
+				$refer_to_site_url = $orgUrl.'.au/';
+				break;
+			case 'JP':
+				$refer_to_site_url = $expUrls.'.co.jp/';
+				break;
+			case 'DE':
+				$refer_to_site_url = $orgUrl.'/lr-de/';
+				break;
+			case 'ES':
+				$refer_to_site_url = $orgUrl.'/lr-es/';
+				break;
+			case 'IT':
+				$refer_to_site_url = $orgUrl.'/lr-it/';
+				break;
+			case 'CN':
+				$refer_to_site_url = $orgUrl.'.cn/';
+				break;
+			default:
+				$refer_to_site_url = $orgUrl;
+		} 
 
-	?>
-	<!--<a class="btn" href="javascript:void(0)" id="liveRampPopup">Pop Up</a>-->
-	<div class="liveRampOverlay v-center">
-	  <div id="liveRamp" class="modal-box liveRamp-modal-box">
-		<div class="modal-body">
-		  <a href="#" class="closeBtn dismissGeoModel"><img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/popup-closeImg.png"></a>
-		  <a href="#"><img class="logoImg" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/popup-logo.png"></a>
-		  <h3>Welcome to LiveRamp.com</h3>
-		  <img class="underlineImg" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/popup-underline.png">
-		  <p>It looks as though you are visiting from the <?php echo $countryLabelList[$countryCode]; ?>.</br> Would you like to visit our <?php echo $countryLabelList[$countryCode]; ?> site?</p>
-		  <div class="btnWrap">
-			<div class="leftBox">
-			  <a href="<?php echo $refer_to_site_url; ?>">Yes, please!</a>
-			  <p>Take me to the <?php echo $countryLabelList[$countryCode]; ?> site</p>
+		?>
+		<!--<a class="btn" href="javascript:void(0)" id="liveRampPopup">Pop Up</a>-->
+		<div class="liveRampOverlay v-center">
+		<div id="liveRamp" class="modal-box liveRamp-modal-box">
+			<div class="modal-body">
+			<a href="#" class="closeBtn dismissGeoModel"><img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/popup-closeImg.png"></a>
+			<a href="#"><img class="logoImg" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/popup-logo.png"></a>
+			<h3>Welcome to LiveRamp.com</h3>
+			<img class="underlineImg" src="<?php echo get_template_directory_uri() ?>/dist/assets/images/popup-underline.png">
+			<p>It looks as though you are visiting from the <?php echo $countryLabelList[$countryCode]; ?>.</br> Would you like to visit our <?php echo $countryLabelList[$countryCode]; ?> site?</p>
+			<div class="btnWrap">
+				<div class="leftBox">
+				<a href="<?php echo $refer_to_site_url; ?>">Yes, please!</a>
+				<p>Take me to the <?php echo $countryLabelList[$countryCode]; ?> site</p>
+				</div>
+				<div class="leftBox">
+				<a href="javascript:void(0)" class="dismissGeoModel">No, thanks.</a>
+				<p>Continue to the US site</p>
+				</div>
 			</div>
-			<div class="leftBox">
-			  <a href="javascript:void(0)" class="dismissGeoModel">No, thanks.</a>
-			  <p>Continue to the US site</p>
 			</div>
-		  </div>
 		</div>
-	  </div>
-	</div>
+		</div>
 
-	<script type="text/javascript">
-	$(function () {
-		/** display geop-popup : code starts **/
-		setTimeout(function(){ 
-			$(".liveRampOverlay").fadeTo(500 , 1);
-			$('#liveRamp').addClass('popupScale');
-			$('body , html').css('overflow','hidden');
-		 }, 2000);
-		/** display geop-popup : code ends **/
-	  $(document).on('click', '.dismissGeoModel', function () {
-		closeGeoModel()
-	  }); 
-	});
-	
-	  function closeGeoModel(){ 
-		$('.liveRampOverlay').fadeOut(500);
-		$(".modal-box").removeClass('popupScale');
-		$('body , html').css('overflow','auto');
-	  }
-	</script>
-<?php } ?>
+		<script type="text/javascript">
+		$(function () {
+			/** display geop-popup : code starts **/
+			setTimeout(function(){ 
+				$(".liveRampOverlay").fadeTo(500 , 1);
+				$('#liveRamp').addClass('popupScale');
+				$('body , html').css('overflow','hidden');
+			}, 2000);
+			/** display geop-popup : code ends **/
+		$(document).on('click', '.dismissGeoModel', function () {
+			closeGeoModel()
+		}); 
+		});
+		
+		function closeGeoModel(){ 
+			$('.liveRampOverlay').fadeOut(500);
+			$(".modal-box").removeClass('popupScale');
+			$('body , html').css('overflow','auto');
+		}
+		</script>
+<?php } } ?>
 <script type="text/javascript">
 	$(function () {
-		var countryCode = '<?php echo $ipdat->geoplugin_countryCode; ?>';
+		var countryCode = '<?php echo $countryCode; ?>';
+		var curPageURL = '<?php echo $curPageURL; ?>';
+		//var network_home_url = '<?php echo $network_home_url; ?>';
+		var orgUrl = '<?php echo $orgUrl; ?>';
 		console.log(countryCode);
-		console.log('Test')
+		console.log('main_site: '+orgUrl+' curPageURL: '+curPageURL);
 	})
 </script>
