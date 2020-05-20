67467970f0f9c2cbe1645e12aa79b69f2f793211
Kumar Desai <kumar.desai@liveramp.com>
2020-05-20 16:02:22 -0700

    added hreflang code

--- wp-content/themes/liveRamp-Template/library/hreflang.php
+++ wp-content/themes/liveRamp-Template/library/hreflang.php
@@ -1,8 +1,7 @@
-<?php
-if( have_rows('links') ):
-    while ( have_rows('links') ) : the_row();
-        ?><link rel="<?php echo the_sub_field('type');?>" href="<?php echo the_sub_field('href');?>" hreflang="<?php echo the_sub_field('language');?>" />
-        <?php
-    endwhile;
-endif;
-?>
+<?php if( have_rows('hreflang') ) { ?>
+	
+	<?php while ( have_rows('hreflang') ) { the_row();?>
+		<link rel="alternate" href="<?php echo the_sub_field('url');?>" hreflang="<?php echo the_sub_field('locale');?>" />
+	<?php }  ?>
+
+<?php } ?>
