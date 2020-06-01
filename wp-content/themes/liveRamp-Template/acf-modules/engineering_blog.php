0da1c1b8d5f70af49da2c4fb50ae6946d28707af
Kumar Desai <kumar.desai@liveramp.com>
2020-06-01 13:58:09 -0700

    engineering blog fixed number issue

--- wp-content/themes/liveRamp-Template/acf-modules/engineering_blog.php
+++ wp-content/themes/liveRamp-Template/acf-modules/engineering_blog.php
@@ -21,8 +21,8 @@
 						
 						// WP_Query arguments
 						$args = array(
-							'post_type'              => array( 'engineering' ),
-							'posts_per_page'         => '6',
+							'post_type'      => array( 'engineering' ),
+							'posts_per_page' => '6',
 						);
 
 						// The Query
@@ -31,13 +31,9 @@
 						// The Loop
 						if ( $wp_query->have_posts() ) {
 							while ( $wp_query->have_posts() ) {
-								$wp_query->the_post(); ?>
-								
-								<?php include('engineering_parts/engineering_card.php') ?>
-
-						<?php	
+								$wp_query->the_post(); 
+								include('engineering_parts/engineering_card.php');
 							}
-
 						} else {
 							// no posts found
 						}
@@ -51,14 +47,14 @@
 				<div id="more-button">
 					<?php if (  $wp_query->max_num_pages > 1 ) : // don't display the button if there are not enough posts ?>
 						<div class="text-center pad-1">
-								<div class="button engineering_loadmore down-arrow">More posts</div>
-							</div>
+							<div class="button engineering_loadmore down-arrow">More posts</div>
+						</div>
 					<?php endif; ?>	
 				</div>
 					
 				<script>
 					var posts_myajax = '<?php echo serialize( $wp_query->query_vars ) ?>',
-				    current_page_myajax = 2,
+				    current_page_myajax = 1,
 				    max_page_myajax = <?php echo $wp_query->max_num_pages ?>
 				</script>
 
