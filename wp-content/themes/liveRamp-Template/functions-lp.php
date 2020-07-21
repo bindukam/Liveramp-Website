be44d4b96a743a1f4e1ba6319d232a5bce6a9f0f
David J. Smario <david.smario@herodigital.com>
2020-07-21 12:31:20 -0700

    Custom window title for LP pages, noindex

--- wp-content/themes/liveRamp-Template/functions-lp.php
+++ wp-content/themes/liveRamp-Template/functions-lp.php
@@ -11,6 +11,24 @@
             return '<iframe class="ss-iframe" src="https://www.slideshare.net/slideshow/embed_code/' . $result[1]. '" frameborder="0" marginwidth="0" marginheight="0" scrolling="no"></iframe>';
         } else {
             return '';
-            
+
         }
 	}
+
+    function lrlp_custom_title($title){
+
+        $post = get_post();
+
+        if(! empty( $post )) {
+            $post_template = get_page_template_slug($post);
+            if($post_template == "page-lp.php") {
+                $page_title = get_field('page_title', $post->ID).'';
+                $site_title = get_bloginfo('name');
+                $title = $page_title != '' ? $site_title.' | '.$page_title : '';
+            }
+        }
+
+        return $title;
+    }
+    add_filter('wpseo_title','lrlp_custom_title', 10, 1);
+    add_filter('pre_get_document_title','lrlp_custom_title');
--- wp-content/themes/liveRamp-Template/header-lp.php
+++ wp-content/themes/liveRamp-Template/header-lp.php
@@ -20,7 +20,8 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		
+        <meta name="robots" content="noindex">	
+
 		<?php include ('library/hreflang.php') ?>
 
 		<?php the_field('drift_code', 'option') ?>
