8a280f34d188ce9bef46ca31c8f0c3c41b5f02eb
David J. Smario <david.smario@herodigital.com>
2020-08-04 11:24:10 -0700

    LP Event Hero component

--- wp-content/themes/liveRamp-Template/acf-lp-modules/event_hero.php
+++ wp-content/themes/liveRamp-Template/acf-lp-modules/event_hero.php
@@ -1,49 +1,77 @@
 <?php
-
-$eyebrow = get_sub_field('eyebrow');
-
 $theme_uri = get_stylesheet_directory();
 $theme_images = $theme_uri.'/dist/assets/images';
 $theme_svg = $theme_images.'/svg';
 
+$hero_image = get_sub_field('hero_image');
 ?>
 
-<section class="lp-event-hero  primary-bkg">
+<section class="lp-event-hero">
+    <div class="hero-image" style="background-image: url(<?php echo $hero_image; ?>);"></div>
     <div class="grid-container ">
         <div class="grid-x grid-margin-x align-justify">
-            <div class="cell green-bkg large-4">
+            <div class="cell large-4">
                 <div class="header-logo">
                     <a href="<?php echo site_url(); ?>" rel="nofollow" aria-label="<?php bloginfo( 'name' ); ?>"><?php echo file_get_contents("$theme_svg/lr_logo.svg"); ?></a> 
                 </div>
             </div>
         </div>
     </div>
-    <div class="grid-container">
-        <div class="grid-x grid-margin-x align-justify">
-            <div class="cell large-4 content">
-                <?php if (get_sub_field('title')): ?>
-                    <h1 class="headline green"><?php the_sub_field('title') ?></h1>
-                <?php endif ?>
-                <?php if (get_sub_field('subheadline')): ?>
-                    <div class="h3 bold green subheadline"><?php the_sub_field('subheadline') ?></div>
-                <?php endif ?>
-                <?php if (get_sub_field('description')): ?>
-                    <div class="copy green"><?php the_sub_field('description') ?></div>
-                <?php endif ?>
-            </div>
-            <div class="cell large-7 form-cell">
-                <div data-sticky-container>
-                    <div class="form-wrapper box-shadow-over-white b-radius white-bkg">
-                        <div class="form-image">
-                            <?php echo wp_get_attachment_image( get_sub_field('image'), 'full', false, array( "class" => "b-radius")); ?>
-                        </div>
-                        <?php if ($cta_text !== '' && $cta_url !== ''): ?>
-                            <div class="cta">
-                                <a href="<?php echo $cta_url ?>" class="button text white cta" target="<?php echo $cta_target ?>"><?php echo $cta_text?></a>
+    <div class="grid-container grid-bottom">
+        <div class="grid-x grid-margin-x">
+            <div class="cell large-12">
+                <div class="float-bottom primary-bkg">
+                    <?php if (get_sub_field('eyebrow_text')): ?>
+                    <div class="eyebrow green">
+                        <?php the_sub_field('eyebrow_text') ?>
+                    </div>
+                    <?php endif ?>
+                    <div class="content">
+                        <div class="col">
+                            <?php if (get_sub_field('title')): ?>
+                            <h1 class="headline green">
+                                <?php the_sub_field('title') ?>
+                            </h1>
+                            <?php endif ?>
+                            <?php if (get_sub_field('sponsor_text') && get_sub_field('sponsor_logo')): ?>
+                            <div class="sponsor green">
+                                <span><?php the_sub_field('sponsor_text') ?></span>
+                                <span class="icon"><img src="<?php the_sub_field('sponsor_logo') ?>" alt="<?php the_sub_field('sponsor_text') ?>" /></span>
                             </div>
-                        <?php endif ?>
+                            <?php endif ?>
+                        </div>
+                        <div class="col">
+                            <?php if (get_sub_field('subheadline')): ?>
+                            <div class="h3 bold green subheadline">
+                                <?php the_sub_field('subheadline') ?>
+                            </div>
+                            <?php endif ?>
+                            <?php if (get_sub_field('description')): ?>
+                            <div class="copy green">
+                                <?php the_sub_field('description') ?>
+                            </div>
+                            <?php endif ?>
+                            <?php 
+                            $booth_url = get_sub_field('cta_find_our_booth')['url'];
+                            $booth_title = get_sub_field('cta_find_our_booth')['title'];
+                            $booth_target = get_sub_field('cta_find_our_booth')['target'];
+                            if ($booth_url): ?>
+                            <div class="btn-row">
+                                 <a href="#" class="button button-white booth">Find our booth</a>
+                            </div>
+                            <?php endif ?>
+                            <?php if (get_sub_field('request_meeting')): ?>
+                            <div class="btn-row">
+                                 <a href="#" class="button cta solid-white register">Request a meeting</a>
+                            </div>
+                            <?php endif ?>
+                        </div>
                     </div>
                 </div>
+            </div>
+        </div>
+        <div class="grid-x grid-margin-x">
+            <div class="cell large-5">
             </div>
         </div>
     </div>
