7236377b47dcac7e8a4ad9c6a6a955e4d60217d0
David J. Smario <david.smario@herodigital.com>
2020-08-05 12:39:41 -0700

    LP Event Form component

--- wp-content/themes/liveRamp-Template/acf-lp-modules/event_form.php
+++ wp-content/themes/liveRamp-Template/acf-lp-modules/event_form.php
@@ -1,89 +1,36 @@
 <?php
-
-$eyebrow = get_sub_field('eyebrow');
-
+    $theme_uri = get_template_directory_uri();
+    $theme_images = $theme_uri.'/dist/assets/images';
+    $theme_svg = $theme_images.'/svg';
+    
+    $img = get_sub_field('event_image');
+    if($img) {
+        $style = " background-image:url('".$img."') ";
+    }
 ?>
 
-<section class="lp-event-form pad-section">
-    <div class="grid-container">
-        <div class="grid-x grid-margin-x align-justify">
-            <div class="cell  large-6 content">
-                <?php if (get_sub_field('title')): ?>
-                <h2 class="title">
-                    <?php the_sub_field('title') ?>
-                </h2>
-                <?php endif ?>
-                <?php if (get_sub_field('description')): ?>
-                <div class="desc">
-                    <?php the_sub_field('description') ?>
-                </div>
-                <?php endif ?>
-
-
-                <?php if (get_sub_field('testimonial_quote')): ?>
-                <div class="quote">
-                    <div class="quote-text">
-                        <?php the_sub_field('testimonial_quote') ?>&rdquo;
-                    </div>
-                    <?php if (get_sub_field('testimonial_name')): ?>
-                    <div class="quote-name">
-                        <?php the_sub_field('testimonial_name') ?>
-                    </div>
+<section class="lp-event-form overlay-me">
+	<div class="grid-container module">
+        <div class="grid-x grid-padding-x">
+			<div class="cell large-6 left-col" style="<?php echo $style; ?>">
+                <div class="block primary-bkg">
+                    <?php if (get_sub_field('title')): ?>
+                    <h2 class="green"><?php the_sub_field('title') ?></h2>
                     <?php endif ?>
-                    <?php if (get_sub_field('testimonial_logo')): ?>
-                    <div class="quote-logo">
-                        <?php echo wp_get_attachment_image( get_sub_field('testimonial_logo'), 'full', '',array( "class" => "circle-image" ) ); ?>
-                    </div>
+                    <?php if (get_sub_field('description')): ?>
+                    <div class="green"><?php the_sub_field('description') ?></div>
                     <?php endif ?>
                 </div>
-                <?php endif ?>
-            </div>
-            <div class="cell large-5">
-                <?php if (get_sub_field('list_headline')): ?>
-                <h3 class="list-headline">
-                    <?php the_sub_field('list_headline') ?>
-                </h3>
-                <div class="fixed-underline">
-                    <img src="<?php echo get_template_directory_uri() ?>/dist/assets/images/svg/title-underline.svg" alt="" >
-                </div>
-                <?php endif ?>
-                <?php if (have_rows('list')): ?>
-                <?php while(have_rows('list')) : ?>
-                <?php the_row(); ?>
+			</div>
+			<div class="cell large-6 right-col form-cell">
                 <?php
-                        $headshot = '';
-                        $logo = '';
-                        $name = '';
-                        $job_title = '';
-                        if (get_sub_field('company_logo')) {
-                            $logo = get_sub_field('company_logo');
-                        }
-                        if (get_sub_field('name')) {
-                            $name = get_sub_field('name');
-                        }
-                        if (get_sub_field('job_title')) {
-                            $job_title = get_sub_field('job_title');
-                        }
+                    $gf_id = get_sub_field('register_gravity_form_id');
+                    if($gf_id) {
+                        gravity_form($gf_id, false, false, false, null, false, 12);
+                    }
                 ?>
-                <div class="list-item">
-                    <div class="item-col">
-                        <div class="circle">
-                            <?php echo wp_get_attachment_image( get_sub_field('headshot'), 'full', false, array( "class" => "circle-image" ) ); ?>
-                        </div>
-                    </div>
-                    <div class="item-col">
-                        <div class="speaker-name">
-                            <?php echo $name; ?>
-                        </div>
-                        <div class="copy">
-                            <?php echo $job_title; ?>
-                        </div>
-                        <?php echo wp_get_attachment_image( get_sub_field('company_logo'), 'full' ); ?>
-                    </div>
-                </div>
-                <?php endwhile ?>
-                <?php endif ?>
             </div>
         </div>
-    </div>
+	</div>
 </section>
+<div class="lp-form-footer-overlay"></div>
