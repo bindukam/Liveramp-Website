f00631b0bff52084d368c1cbad025e3b15a7b7f3
Kumar Desai <kumar.desai@liveramp.com>
2020-08-24 11:26:11 -0700

    Update header.php
    Removed hardcoded Bugherd Teco link and old slick.css link

--- wp-content/themes/liveRamp-Template/header.php
+++ wp-content/themes/liveRamp-Template/header.php
@@ -43,8 +43,6 @@
 
 		<?php wp_head(); ?>
 		
-		<!-- <link rel="stylesheet" type="text/css" href="<?php echo $theme_uri; ?>/dist/assets/js/slick.css"/> -->
-
 		<!-- CSS code -->
 		<style><?php the_field('css_code', 'option') ?></style>
 		<!-- End CSS code -->		
@@ -52,8 +50,6 @@
 		<!-- Bugherd Code  -->
 		<?php the_field('bugherd_code', 'option') ?>
 		<!-- End Bugherd Code -->
-
-		<script type="text/javascript" src="https://www.bugherd.com/sidebarv2.js?apikey=boxtwtjwrzcua9blxferxg" async="true"></script>
 
 	</head>
 <body <?php body_class(); ?>>
