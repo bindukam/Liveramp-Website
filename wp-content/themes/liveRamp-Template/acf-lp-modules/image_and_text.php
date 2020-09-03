75fbe42fb56ea5060a3f4c90fb804e570a1398ea
David J. Smario <david.smario@herodigital.com>
2020-09-03 14:43:01 -0700

    LR-22 LP Image and Text

--- wp-content/themes/liveRamp-Template/acf-lp-modules/image_and_text.php
+++ wp-content/themes/liveRamp-Template/acf-lp-modules/image_and_text.php
@@ -4,29 +4,18 @@
 	if (get_sub_field('image')):
 		$hide = '';
 		$center = '';
-		$size = 'medium-6';
+		$size = 'large-6';
 	else:
 		$hide = ' hide';
 		$center = ' align-center text-center no-image';
 		$size = 'medium-9';
 	endif;
-
-	if (get_sub_field('flip_module') === true):
-		$orderContent = 'medium-order-2';
-		$orderImage = 'medium-order-1';
-		$flipped = ' flipped';
-	else:
-		$orderContent = 'medium-order-1';
-		$orderImage = 'medium-order-2';
-		$flipped = '';
-	endif;
-
 	?>
 	
 
 	<div class="grid-container">
-		<div class="grid-x grid-padding-x align-top<?php echo $center; echo $flipped; ?>">
-			<div class="<?php echo $size ?> cell content small-order-2 <?php echo $orderContent ?>">
+		<div class="grid-x grid-padding-x align-top<?php echo $center; ?>">
+			<div class="<?php echo $size ?> cell content small-order-1 medium-order-1">
 				
 				<?php if (get_sub_field('title')): ?>
 					<div class="title_area">
@@ -54,7 +43,7 @@
 				<a href="<?php echo $url ?>" class="button cta outline tag2" target="<?php echo $target ?>"><?php echo $title ?></a>
 				<?php endif ?>
 			</div>
-			<div class="cell align-top medium-6 image-container small-order-1 <?php echo $orderImage . $hide ?>">
+			<div class="large-6 cell align-top image-container small-order-2 medium-order-2 <?php echo $hide ?>">
 				<div class="image-area">
                     <?php echo wp_get_attachment_image( get_sub_field('image'), 'full', false, array( "class" => "b-radius tab-image" ) ); ?>
 				</div>
