939c1c3065ea19b06208a3dc31724b39f98d69b4
David J. Smario <david.smario@herodigital.com>
2020-09-04 15:24:12 -0700

    LR-26 - LP Media Component

--- wp-content/themes/liveRamp-Template/acf-lp-modules/media.php
+++ wp-content/themes/liveRamp-Template/acf-lp-modules/media.php
@@ -1,62 +1,51 @@
 <?php
-
-$theme_uri = get_stylesheet_directory();
-$theme_images = $theme_uri.'/dist/assets/images';
-$theme_svg = $theme_images.'/svg';
-
+    $media_type = get_sub_field('media_type');
+    $video_id = get_sub_field('video_id');
+    $ss = get_sub_field('slideshare_ss');
 ?>
-
-<section class="hero-with-form primary-bkg media">
-    <div class="grid-container ">
-        <div class="grid-x grid-margin-x align-justify">
-            <div class="cell green-bkg large-4 small-6">
-                <div class="header-logo">
-                    <a href="<?php echo site_url(); ?>" rel="nofollow" aria-label="<?php bloginfo( 'name' ); ?>"><?php echo file_get_contents("$theme_svg/lr_logo.svg"); ?></a> 
+<section class="hero-with-form media-section">
+    <div class="media-wrapper primary-bkg <?php echo get_sub_field('background_pattern'); ?>">
+        <div class="grid-container ">
+            <div class="grid-x grid-margin-x align-justify">
+                <div class="cell green-bkg large-4 small-6">
+                    <div class="header-logo">
+                        <a href="<?php echo site_url(); ?>" rel="nofollow" aria-label="<?php bloginfo( 'name' ); ?>"><img src="<?php echo get_sub_field('logo'); ?>"></a> 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="grid-container">
-        <div class="grid-x grid-margin-x align-justify">
-            <div class="cell large-5 form-cell">
-                        <div class="cell eyebrow">
-                            <div class="icon"><img src="<?php echo get_sub_field('eyebrow_icon'); ?>"></div>
-                            <div class="copy green"><?php echo get_sub_field('eyebrow_text'); ?></div>
+        <div class="grid-container">
+            <div class="grid-x grid-margin-x align-justify">
+                <div class="cell large-5 form-cell">
+                    <div class="cell eyebrow">
+                        <div class="icon"><img src="<?php echo get_sub_field('eyebrow_icon'); ?>"></div>
+                        <div class="copy green"><?php echo get_sub_field('eyebrow_text'); ?></div>
+                    </div>
+                    <?php if (get_sub_field('title')): ?>
+                        <h1 class="headline green"><?php the_sub_field('title') ?></h1>
+                    <?php endif ?>
+                    <?php if (get_sub_field('subheadline')): ?>
+                        <div class="h3 bold green subheadline"><?php the_sub_field('subheadline') ?></div>
+                    <?php endif ?>
+                    <?php if (get_sub_field('description')): ?>
+                        <div class="copy green"><?php the_sub_field('description') ?></div>
+                    <?php endif ?>
+                </div>
+                <div class="cell large-5 form-cell">
+                    <div data-sticky-container>
+                        <div class="form-wrapper box-shadow-over-white b-radius white-bkg">
+                            <?php
+                                $gf_id = get_sub_field('gravity_form_id');
+                                if($gf_id) {
+                                    gravity_form($gf_id, false, false, false, null, true, 12);
+                                }
+                            ?>
                         </div>
-                        <?php if (get_sub_field('title')): ?>
-                            <h1 class="headline green"><?php the_sub_field('title') ?></h1>
-                        <?php endif ?>
-                        <?php if (get_sub_field('subheadline')): ?>
-                            <div class="h3 bold green subheadline"><?php the_sub_field('subheadline') ?></div>
-                        <?php endif ?>
-                        <?php if (get_sub_field('description')): ?>
-                            <div class="copy green"><?php the_sub_field('description') ?></div>
-                        <?php endif ?>
-            </div>
-            <div class="cell large-5 form-cell">
-                <div data-sticky-container>
-                    <div class="form-wrapper box-shadow-over-white b-radius white-bkg">
-                        <?php
-                            $gf_id = get_sub_field('gravity_form_id');
-                            if($gf_id) {
-                                gravity_form($gf_id, false, false, false, null, true, 12);
-                            }
-                        ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</section>
-
-
-<?php
-    $media_type = get_sub_field('media_type');
-    $video_id = get_sub_field('video_id');
-    $ss = get_sub_field('slideshare_ss');
-?>
-<section class="hero-with-form media-bottom-section">
-    <div class="hero-with-form primary-bkg media media-float"></div>
     <div class="grid-container media-video">
         <div class="grid-x grid-margin-x align-justify">
             <div class="cell ">
