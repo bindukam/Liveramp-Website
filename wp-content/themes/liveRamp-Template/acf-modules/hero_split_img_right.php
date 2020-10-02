fa02de30007e261210883771ec7b749d3f4774f4
Kumar Desai <kumar.desai@liveramp.com>
2020-10-02 10:09:06 -0700

    New modules 1

---
+++ wp-content/themes/liveRamp-Template/acf-modules/hero_split_img_right.php
@@ -0,0 +1,43 @@
+<!-- new092920 -->
+<section class="hero-split-img-right">
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="cell large-8" style="background-color: red; height: 150px"></div>
+			<div class="cell large-4" style="background-color: green; height: 150px"></div>
+		</div>
+	</div>
+</section>	
+<section class="hero-split-img-right medium-blue-bkg">
+	<div class="grid-container">
+		<div class="grid-x">
+			<div class="cell large-8 split-text">
+				<img src="https://assets.website-files.com/5f24978228a63bcebae76027/5f4ef9215d19fdd13f356aee_icon_hero_trust-infrastructure%402x.png">
+				<h1>Safe Haven: Trust Infrastructure for a New Era</h1>
+				
+				<ul>
+					<li>
+						<img src="https://assets.website-files.com/5f24978228a63bcebae76027/5f4ef139636fbbce060c91c4_icon_hero_checkmark%402x.png">
+						<p>Configurable collaboration</p>
+					</li>
+					<li>
+						<img src="https://assets.website-files.com/5f24978228a63bcebae76027/5f4ef139636fbbce060c91c4_icon_hero_checkmark%402x.png">
+						<p>Privacy-conscious connectivity</p>
+					</li>
+					<li>
+						<img src="https://assets.website-files.com/5f24978228a63bcebae76027/5f4ef139636fbbce060c91c4_icon_hero_checkmark%402x.png">
+						<p>Continuous control of data</p>
+					</li>
+					<li>
+						<img src="https://assets.website-files.com/5f24978228a63bcebae76027/5f4ef139636fbbce060c91c4_icon_hero_checkmark%402x.png">
+						<p>Ecosystem actionability</p>
+					</li>
+				</ul>
+			</div>
+			<div class="cell large-4 split-image">
+				<div class="img-area">
+					<img src="https://assets.website-files.com/5f24978228a63bcebae76027/5f3dc92908a3af39d09e9407_Safe-haven_img_hero%402x_compressed.jpg" alt="Safe Haven: Trust Infrastructure for a New Era" srcset="https://assets.website-files.com/5f24978228a63bcebae76027/5f3dc92908a3af39d09e9407_Safe-haven_img_hero%402x_compressed-p-1080.jpeg 1080w, https://assets.website-files.com/5f24978228a63bcebae76027/5f3dc92908a3af39d09e9407_Safe-haven_img_hero%402x_compressed-p-1600.jpeg 1600w, https://assets.website-files.com/5f24978228a63bcebae76027/5f3dc92908a3af39d09e9407_Safe-haven_img_hero%402x_compressed.jpg 2000w" sizes="(max-width: 991px) 100vw, 40vw" class="object-fit--cover">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>	
---
+++ wp-content/themes/liveRamp-Template/page-new-module-test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: New Module Test Page
+*/
+
+
+get_header(); ?>
+
+<?php 
+	include ('acf-modules/hero_split_img_right.php');
+ ?>
+
+<?php
+get_footer();
