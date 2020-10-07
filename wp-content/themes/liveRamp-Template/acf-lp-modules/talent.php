d66b8421472dcebf80ba84c2eae4e36a4e70cd5e
David J. Smario <david.smario@herodigital.com>
2020-10-07 15:22:57 -0700

    Bugherd Task #68 - test talent headshot on phone

--- wp-content/themes/liveRamp-Template/acf-lp-modules/talent.php
+++ wp-content/themes/liveRamp-Template/acf-lp-modules/talent.php
@@ -67,7 +67,7 @@ $eyebrow = get_sub_field('eyebrow');
                 ?>
                 <div class="list-item">
                     <div class="item-col">
-                        <div class="circle">x<img class="circle-image" src="<?php echo get_sub_field('headshot'); ?>" />x</div>
+                        <div class="circle"><img class="circle-image" src="<?php echo get_sub_field('headshot'); ?>" /></div>
                     </div>
                     <div class="item-col">
                         <div class="speaker-name">
