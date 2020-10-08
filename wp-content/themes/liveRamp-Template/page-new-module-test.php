d6796b1748d12ab5c6ce9756d28618ce4c24fa65
Kumar Desai <kumar.desai@liveramp.com>
2020-10-08 13:34:18 -0700

    new modules

--- wp-content/themes/liveRamp-Template/page-new-module-test.php
+++ wp-content/themes/liveRamp-Template/page-new-module-test.php
@@ -6,11 +6,12 @@
 
 <?php get_header(); ?>
 
-<?php include ('acf-modules/hero_text_left_image_right.php'); ?>
+<?php //include ('acf-modules/hero_text_left_image_right.php'); ?>
 
-<?php include ('acf-modules/written_story_centered.php'); ?>
+<?php //include ('acf-modules/written_story_centered.php'); ?>
 
-<?php include ('acf-modules/icon_line_title.php'); ?>
+<?php //include ('acf-modules/icon_line_title.php'); ?>
 
+<?php include ('acf-modules/icon_text_left_right.php'); ?>
 
 <?php get_footer(); ?>
