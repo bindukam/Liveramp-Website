c99024094cde1e61cd0f1cb1e8da8f59d4ace4b1
Neelam Thakur <38979502+nvish-neelamthakur@users.noreply.github.com>
2021-01-21 23:57:02 +0530

    Nav bar mobile changes

--- wp-content/themes/liveRamp-Template/acf-modules/secondary_navigation_bar.php
+++ wp-content/themes/liveRamp-Template/acf-modules/secondary_navigation_bar.php
@@ -1,6 +1,7 @@
 <?php if (have_rows('nav-items')): ?>
-	<section class="secondary_navigation_bar pad-section" id="secondaryNavBar">
-		<ul class="sec_nav_bar_wrapper">
+	<section class="secondary_navigation_bar pad-section secondary-nav" id="secondaryNavBar">
+			<div id="selected-item"></div>
+			<ul id="secondary-nav-items" class="sec_nav_bar_wrapper">
 			<?php while(have_rows('nav-items')) : the_row();  ?>
 					<?php
 						$is_cta = false;
@@ -25,15 +26,42 @@
 
 			<?php endwhile; ?>
 		</ul>
+		<a id="nav-show-hide" href="javascript:void(0);" class="icon" onclick="navshowhide()">
+  			<i id="nav-arrow" class="fas fa-angle-up"></i>
+ 		 </a>
 	</section>
 <?php endif; ?>
 <script type="text/javascript">
+//var activeitem = document.querySelector('ul#myLinks li.active').textContent;
+//console.log(activeitem);
+function navshowhide() {
+  var x = document.getElementById("secondary-nav-items");
+  if (x.style.display === "block") {
+	var element = document.getElementById("nav-arrow");
+	element.classList.add("fa-angle-up");
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+	var element = document.getElementById("nav-arrow");
+	element.classList.add("fa-angle-down");
+  }
+}
+
+/*$('ul#secondary-nav-items li').on('click', function(e) {
+      // alert($(this).text());
+	  var si = $(this).text();
+	   $('#selected-item').text(si);
+       e.stopPropagation();
+   });*/
+
 /** scroll-down to target element on Nav-Item click **/
 $(".scroll_nav_item").click(function() {
+	$(this).css({"color": "#ffffff", "border-bottom": "none"});
 	let target = $(this).data('target');
 	let topHeaderHeight = $(".sticky-container > .sticky").outerHeight();
 	let secondaryHeaderHeight = $("#secondaryNavBar").outerHeight();
-	
+	console.log('clicktarget'+target);
+	$('#selected-item').text(target);
 	if($("."+target).length){
 		let totalOffset=0
 		
@@ -92,16 +120,19 @@ $(document).ready(function(){
 	   cur = cur[cur.length-1];
 	   
 	   let itarget = cur && cur.length==1 ? cur.attr('class') : "";
-	   console.log('itarget'+itarget)
+	  
 	   // Set/remove active class
 	   if(itarget == ""){
 		menuItems
 		 .parent().removeClass("active")
 		 .end();
+	     $('#selected-item').text('Go To Sections');
 	   }else{
 		menuItems
 		 .parent().removeClass("active")
 		 .end().filter("a[data-target='"+itarget+"']").parent().addClass("active");
+		 var secnavheader = itarget.replace('-', " ");
+	   $('#selected-item').text(secnavheader).css('textTransform', 'capitalize');
 	   }
 		
 	    //Add/Remove sticky class from Nav-bar
