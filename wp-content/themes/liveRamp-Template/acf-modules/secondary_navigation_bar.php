26fd9e5f342d9ebb3b634a6beb229d1e07bf17f1
nvish-neelamthakur <covid@2k20>
2021-02-03 19:16:25 -0500

    updated secondary_navigation_bar.php

--- wp-content/themes/liveRamp-Template/acf-modules/secondary_navigation_bar.php
+++ wp-content/themes/liveRamp-Template/acf-modules/secondary_navigation_bar.php
@@ -1,6 +1,7 @@
 <?php if (have_rows('nav-items')): ?>
-	<section class="secondary_navigation_bar pad-section" id="secondaryNavBar">
-		<ul class="sec_nav_bar_wrapper">
+	<section class="secondary_navigation_bar pad-section secondary-nav" id="secondaryNavBar">
+			<div id="selected-item"></div>
+			<ul id="secondary-nav-items" class="sec_nav_bar_wrapper">
 			<?php while(have_rows('nav-items')) : the_row();  ?>
 					<?php
 						$is_cta = false;
@@ -25,15 +26,41 @@
 
 			<?php endwhile; ?>
 		</ul>
+		<a id="nav-show-hide" href="javascript:void(0);" class="icon" onclick="navshowhide()">
+  			<i id="nav-arrow" class="fas fa-angle-up"></i>
+ 		 </a>
 	</section>
 <?php endif; ?>
 <script type="text/javascript">
+//var activeitem = document.querySelector('ul#myLinks li.active').textContent;
+//console.log(activeitem);
+function navshowhide() {
+  var x = document.getElementById("secondary-nav-items");
+  if (x.style.display === "block") {
+	var element = document.getElementById("nav-arrow");
+	element.classList.add("fa-angle-up");
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+	var element = document.getElementById("nav-arrow");
+	element.classList.add("fa-angle-down");
+  }
+}
+
+/*$('ul#secondary-nav-items li').on('click', function(e) {
+      // alert($(this).text());
+	  var si = $(this).text();
+	   $('#selected-item').text(si);
+       e.stopPropagation();
+   });*/
+
 /** scroll-down to target element on Nav-Item click **/
 $(".scroll_nav_item").click(function() {
 	let target = $(this).data('target');
 	let topHeaderHeight = $(".sticky-container > .sticky").outerHeight();
 	let secondaryHeaderHeight = $("#secondaryNavBar").outerHeight();
-	
+	console.log('clicktarget'+target);
+	$('#selected-item').text(target);
 	if($("."+target).length){
 		let totalOffset=0
 		
@@ -52,6 +79,7 @@ $(".scroll_nav_item").click(function() {
 });
 
 $(document).ready(function(){
+	var oldPadding = $('#secondaryNavBar').next().next().css("padding-top");
 	var topMenu = $(".sec_nav_bar_wrapper"), 
 	 // All nav-bar items
     menuItems = topMenu.find("a.scroll_nav_item"),
@@ -72,7 +100,8 @@ $(document).ready(function(){
 
 	// Get the offset position of the navbar
 	var sticky = navbar.offsetTop;
-
+	
+	
 	// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 	function stickMyBar() {
 		let topHeaderHeight = $(".sticky-container > .sticky").outerHeight();
@@ -92,26 +121,36 @@ $(document).ready(function(){
 	   cur = cur[cur.length-1];
 	   
 	   let itarget = cur && cur.length==1 ? cur.attr('class') : "";
-	   console.log('itarget'+itarget)
+	  
 	   // Set/remove active class
 	   if(itarget == ""){
 		menuItems
 		 .parent().removeClass("active")
 		 .end();
+	     $('#selected-item').text('Introduction');
 	   }else{
 		menuItems
 		 .parent().removeClass("active")
 		 .end().filter("a[data-target='"+itarget+"']").parent().addClass("active");
+		 var secnavheader = itarget.replace('-', " ");
+		$('#selected-item').text(secnavheader).css('textTransform', 'capitalize');
 	   }
 		
 	    //Add/Remove sticky class from Nav-bar
 		if (windowTop + topHeaderHeight > footerBannerOffset) {
 			navbar.classList.remove("secBarSticky");
-		} else if (window.pageYOffset > sticky) {
+			$('#secondaryNavBar').css('top',0);
+			$('#secondaryNavBar').next().next().css('padding-top',oldPadding);
+		} else if (window.pageYOffset - 28 > sticky) {
 				navbar.classList.add("secBarSticky");
 				$('#secondaryNavBar').css('top',topHeaderHeight);
+				let stickyHeight = topHeaderHeight + secondaryHeaderHeight - 28;
+				
+				$('#secondaryNavBar').next().next().css('padding-top',stickyHeight);
 		} else {
 			navbar.classList.remove("secBarSticky");
+			$('#secondaryNavBar').css('top',0);
+			$('#secondaryNavBar').next().next().css('padding-top',oldPadding);
 		}
 	}
 });
